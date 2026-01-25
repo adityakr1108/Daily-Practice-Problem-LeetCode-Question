b70160b05d6b131e1104a0001726585aaec646ff
Aditya Kumar <aditya.kr.agrawal1108@gmail.com>
2026-01-26 04:56:49 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (98.56%) - LeetSync

---
+++ dp/1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int fib(int n) {
+        if(n <= 1) return n;
+       int prev2 = 0;
+        int prev = 1;
+        int curr = 0;
+        for(int i = 2;i<=n;i++){
+            curr = prev2 + prev;
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+}
