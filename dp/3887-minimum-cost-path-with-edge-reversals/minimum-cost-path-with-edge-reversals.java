f393a056f24072389f1e108fc3662b98d47dd478
Aditya Kumar <aditya.kr.agrawal1108@gmail.com>
2026-01-28 00:03:19 +0530

    Time: 133 ms (43.72%) | Memory: 273 MB (55.06%) - LeetSync

---
+++ dp/3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int minCost(int n, int[][] edges) {
+        // created an adjanecy list
+        List<int[]>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
+
+        // add up all the elements with w and 2*w(for reverse one)
+        for (int[] e : edges) {
+            graph[e[0]].add(new int[]{e[1], e[2]});
+            graph[e[1]].add(new int[]{e[0], 2 * e[2]});
+        }
+        
+        long[] dist = new long[n];
+        Arrays.fill(dist, Long.MAX_VALUE);
+        dist[0] = 0;
+
+        PriorityQueue<long[]> pq =
+            new PriorityQueue<>(Comparator.comparingLong(a -> a[0]));
+        pq.offer(new long[]{0, 0});
+
+        while (!pq.isEmpty()) {
+            long[] cur = pq.poll();
+            long cost = cur[0];
+            int node = (int) cur[1];
+
+            if (cost > dist[node]) continue;
+
+            for (int[] nxt : graph[node]) {
+                int v = nxt[0];
+                int w = nxt[1];
+                if (dist[v] > cost + w) {
+                    dist[v] = cost + w;
+                    pq.offer(new long[]{dist[v], v});
+                }
+            }
+        }
+
+        return dist[n - 1] == Long.MAX_VALUE ? -1 : (int) dist[n - 1];
+    }
+}
