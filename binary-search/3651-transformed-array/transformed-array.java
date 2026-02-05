a7fc44b562027884eee85cc1ce32630bef53fbd4
Aditya Kumar <aditya.kr.agrawal1108@gmail.com>
2026-02-05 13:56:42 +0530

    Time: 1 ms (98.72%) | Memory: 46.9 MB (30.77%) - LeetSync

--- binary-search/3651-transformed-array/transformed-array.java
+++ binary-search/3651-transformed-array/transformed-array.java
@@ -3,19 +3,11 @@ class Solution {
         int n = nums.length;
         int result[] = new int[n];
         for(int i = 0;i<n;i++){
-            if(nums[i] > 0){
-                result[i] = nums[(i+nums[i])%n];
+            int idx = (i + nums[i])%n;
+            if(idx<0){
+                idx+=n;
             }
-            else if(nums[i] < 0){
-                int idx = (i - Math.abs(nums[i]))%n;
-                if(idx < 0){
-                    idx += n;
-                }
-                result[i] = nums[idx];
-            }
-            else{
-                result[i] = nums[i];
-            }
+            result[i] = nums[idx];
         }
         return result;
     }
